472eac858c09a10e9a037c3550df3a3e150b939f
khuyen97 <khanhhuyentb97@gmail.com>
2018-04-22 06:48:00 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :comments
+  resource :user, only: [:edit, :update]
+  get 'pages/info'
+  resources :people
+  resources :ideas
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root 'ideas#index'
+  get 'pages/info'
+end
